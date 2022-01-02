cdd81527d8e39ebaad7d5e397f510e5c10f8f970
shohag06 <shohag.cse06@gmail.com>
2022-01-01 17:56:56 -0800

    Add project files.

---
+++ SimpleCrudApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SimpleCrudApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
